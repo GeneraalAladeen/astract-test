cdf531de478e9ee6320f209cbefc8f74c77008df
GeneraalAladeen <32127065+GeneraalAladeen@users.noreply.github.com>
2022-08-13 17:42:16 +0100

    login restriction for pending users

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
@@ -22,11 +23,70 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+
     /**
-     * Where to redirect users after login.
+     * Create a new controller instance.
      *
-     * @var string
+     * @return void
      */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+
+
+    /**
+    * Handle a login request to the application.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+    *
+    * @throws \Illuminate\Validation\ValidationException
+    */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            if ($request->hasSession()) {
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            if ($request->user()->status === 'pending' && !$request->user()->isAdmin()) {
+                $this->logout($request);
+
+                return back()->withErrors([
+                    'email' => 'This account is pending and requires approval from admin',
+                ]);
+            }
+
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
+    * Where to redirect users after login.
+    *
+    * @var string
+    */
     public function redirectTo(): string
     {
         $role = Auth::user()->role;
@@ -42,15 +102,5 @@ class LoginController extends Controller
                 return '/home';
                 break;
         }
-    }
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
     }
 }
