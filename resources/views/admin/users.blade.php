37854345073ebfc9a2b73a33cecb1f71e9d45d81
GeneraalAladeen <32127065+GeneraalAladeen@users.noreply.github.com>
2022-08-14 21:46:20 +0100

    update db connection

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 col-sm-12">
             <div class="card">
                 <div class="card-body">
                     <form class="form-group" method="GET" action={{ route('admin.dashboard.users')}}>
                         <label><strong>Status :</strong></label>
                         <select id='status' name="status" class="form-control" >
                             <option value="">All</option>
-                            <option value="1" @if( request()->query('status') == 1) selected @endif>Active</option>
-                            <option value="0" @if( request()->query('status') === 0) selected @endif>Pending</option>
+                            <option value="1" @if( request()->query('status') === "1") selected @endif>Active</option>
+                            <option value="0" @if( request()->query('status') === "0") selected @endif>Pending</option>
                         </select>
 
                         <div class="row mt-2">
